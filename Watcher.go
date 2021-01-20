c4c204d6f6d675227cdf1f9c0b748f31450e449f
Jaeyong Park <72537190+jaypyon@users.noreply.github.com>
2021-01-20 16:38:27 +0900

    Create Watcher.go
    리눅스 환경에서의 System call을 이용하여 
    hash.txt 파일에 입력되어있는 파일 해쉬값을 이용하여
    해당 환경에 파일 해쉬값에 해당하는 프로그램이 있는지 확인한다.
    결과는 result.txt에 반환한다.

---
+++ Watcher.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+)
+//import "time"
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func exec_command_sha(hash string) string{
+	fmt.Println("SHA-1 : ",hash)
+	lsCmd := exec.Command("bash", "-c", `find . -type f -exec sh -c '
+   sha1sum "$2" | cut -f 1 -d " " | sed "s|^\\\\||" | grep -Eqi "$1"
+' find-sh "`+ hash +`" {} \; -print`)
+	lsOut, err := lsCmd.Output()
+	check(err)
+
+	return string("SHA-1: "+hash+" : "+string(lsOut)+"\n")
+}
+
+func exec_command_md5(hash string) string{
+	fmt.Println("MD5 : ",hash)
+	lsCmd := exec.Command("bash", "-c", `find . -type f -exec sh -c '
+   md5sum "$2" | cut -f 1 -d " " | sed "s|^\\\\||" | grep -Eqi "$1"
+' find-sh "`+ hash +`" {} \; -print`)
+	lsOut, err := lsCmd.Output()
+	check(err)
+
+	return string("MD5: "+hash+" : "+string(lsOut)+"\n")
+}
+func main() {
+	
+	var hashes = make([]string,0)
+	//fmt.Println(os.Getwd())
+	f, err := os.Open("./hash.txt")
+	check(err)
+	defer f.Close()
+	
+	fo, err := os.Create("result.txt")
+	check(err)
+	defer fo.Close()
+	
+	reader:= bufio.NewReader(f)
+	for{
+		line, isPrefix, err := reader.ReadLine()
+		if isPrefix||err!=nil{
+			break
+		}
+		hashes = append(hashes,string(line))
+		
+	}
+	fmt.Println(hashes)
+	
+	
+	
+	for _,value := range hashes {
+		if len(value)==40{
+			_, err = fo.WriteString(exec_command_sha(value))
+			check(err)
+		}
+		if len(value)==32{
+			_, err = fo.WriteString(exec_command_md5(value))
+			check(err)
+		}
+	}
+	
+}
